acda2775d674f268298e772b3718a1bcc85e75d1
dusai <474377974@qq.com>
2019-06-01 21:52:52 +0800

    debugging

--- article/views.py
+++ article/views.py
@@ -104,11 +104,11 @@ def article_detail(request, article_id):
     :param article_id: 文章的id
     """
     article = ArticlesPost.objects.get(id=article_id)
-    try:
-        article.increase_views()
-    except BaseException as e:
-        print('article_detail_views_error')
-        print(e)
+    # try:
+    #     article.increase_views()
+    # except BaseException as e:
+    #     print('article_detail_views_error')
+    #     print(e)
 
     md = markdown.Markdown(
         extensions=[
