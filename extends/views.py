d2ed71c5815813339130a4b82cf3e5dd70d07cc8
dusai <474377974@qq.com>
2019-06-30 00:49:34 +0800

    developing likes

--- extends/views.py
+++ extends/views.py
@@ -5,6 +5,10 @@ from django.http import JsonResponse
 from .models import SiteMessage
 from comments.models import Comment
 
+from dusainet2.settings import LOGGING
+import logging
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger('django.request')
 
 # 获取最后一条站通知
 def latest_site_message(request):
@@ -27,7 +31,12 @@ def increase_likes(request, obj_type, obj_id):
     if obj_type == 'comment':
         obj = get_object_or_404(Comment, id=obj_id)
     else:
-        return
+        logger.error(
+            'extends increase_likes: get object went wrong.\n    request url: {0}'.format(
+                request.path_info
+            )
+        )
+        return JsonResponse({'state': 500})
     obj.likes += 1
     obj.save(update_fields=['likes'])
     return JsonResponse({'state': 200})
